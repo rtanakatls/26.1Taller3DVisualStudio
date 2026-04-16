ee77a9d5165c22d29989993ba389090555959bdc
Ricardo Tanaka Terukina <rtanaka@talento.tls.edu.pe>
2026-04-16 12:40:14 -0500

    Added enemy melee

---
+++ EnemyMelee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taller3D261
+{
+    internal class EnemyMelee : Enemy
+    {
+        public EnemyMelee(string name, int health) : base(name, health)
+        {
+        }
+    }
+}
